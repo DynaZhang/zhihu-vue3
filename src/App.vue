2dc7b36924c5d53d57b4a70c36529d8e90047183
zhangzhulei <zhangzhulei@tal.com>
2020-11-17 10:54:01 +0800

    临时提交

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <global-header :user="user" />
+  <column-list :list="columnList"/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ColumnList from './components/ColumnList.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import { defineComponent, ref, onMounted } from 'vue'
+
+interface ColumnProp {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    const columnList = ref([] as ColumnProp[])
+    const user = ref({ isLogin: false })
+    onMounted(() => {
+      columnList.value = [
+        {
+          id: 1,
+          title: 'test1的专栏',
+          description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+          avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+        },
+        {
+          id: 2,
+          title: 'test2的专栏',
+          description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+          avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+        }
+      ]
+    })
+    return {
+      columnList,
+      user
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,11 +1,34 @@
 <template>
-$END$
+  <div class="dropdown">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" id="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">{{title}}</a>
+  </div>
+  <ul class="dropdown-menu" aria-labelledby="dropdown-toggle">
+    <li>
+      <a href="#" class="dropdown-item">新建文章</a>
+    </li>
+    <li>
+      <a href="#" class="dropdown-item">我的专栏</a>
+    </li>
+    <li>
+      <a href="#" class="dropdown-item">编辑资料</a>
+    </li>
+    <li>
+      <a href="#" class="dropdown-item">退出登录</a>
+    </li>
+  </ul>
 </template>
 
-<script>
-export default {
-name: "Dropdown"
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
 </script>
 
 <style scoped>
